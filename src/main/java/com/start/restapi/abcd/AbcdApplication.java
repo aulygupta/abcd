3663cc0987d57dcd02513ae2541dc376c6be0f9d
ShreyansJain24 <jainshreyans251@gmail.com>
2022-05-18 20:38:20 +0530

    Update AbcdApplication.java
    descriptive msg in sample file 1st run sucessful

--- src/main/java/com/start/restapi/abcd/AbcdApplication.java
+++ src/main/java/com/start/restapi/abcd/AbcdApplication.java
@@ -8,6 +8,8 @@ public class AbcdApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AbcdApplication.class, args);
-	}
+		System.out.println("hii team lets make the project");
+		
+     }
 
 }
